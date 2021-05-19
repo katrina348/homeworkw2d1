064926f27cfe7b36fb88a1217bfe3a67c609f415
Kat Duff <katrina348@gmail.com>
2021-05-19 08:05:03 -0600

    more changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,21 @@ class PagesController < ApplicationController
     @pages = Page.all
     render component: "Pages", props:{pages:@pages}
   end
+
+  def show
+    @page = Page.find(params[:id])
+    render component: "Page", props:{page:page}
+  end
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to pages_path
+  end
+  def new
+    render component: "PageNew"
+  end
+  def create
+    # create page here
+  end
+
 end
