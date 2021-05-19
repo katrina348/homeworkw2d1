90605eaf84386e11111fec3eb854a304d5ab595a
Kat Duff <katrina348@gmail.com>
2021-05-18 23:20:17 -0600

    note

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
-    render component: "Pages"
+    @pages = Page.all
+    render component: "Pages", props:{pages:@pages}
   end
 end
--- app/javascript/components/pages.js
+++ app/javascript/components/pages.js
@@ -3,11 +3,25 @@ import React from 'react'
 // a react component is just a js function that returns jsx
 
 const Pages = (props) => {
+  const pages = props.pages
+
+  const renderPages = ()=>{
+   return pages.map((page) => {
+     return(
+       <div>
+         <h1>{page.title}</h1>
+         <p>{page.author}</p>
+         <p>{page.id}</p>
+         <p>{page.body}</p>
+       </div>
+     )
+   })
+  }
   return(
     <div>
       <h1>Pages here</h1>
+      { renderPages() }
     </div>
   )
 }
-
 export default Pages
